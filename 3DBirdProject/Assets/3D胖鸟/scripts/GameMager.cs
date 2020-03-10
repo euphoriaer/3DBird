6e38b46399d162fdc583106f69f09e06d877c071
1764183867@qq.com <1764183867@qq.com>
2020-03-10 19:38:22 +0800

    stateGame
    游戏状态

---
+++ 3DBirdProject/Assets/3D胖鸟/scripts/GameMager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class GameMager : MonoBehaviour
+{
+    public Transform onePosition;
+    public Transform twoPosition;
+
+    public Transform threePosition;
+    public GameObject creatGameObject;
+
+
+    public float creatTime;
+    private float timeTemp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeTemp = creatTime;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RangeCreat();
+        creatTime -= Time.deltaTime;
+
+    }
+
+    public void Creat(Transform position)
+    {
+        
+        
+        if (creatTime <= 0)
+        {
+            Instantiate((creatGameObject),position);
+            creatTime = timeTemp;
+
+        }
+       
+
+    }
+
+    public void RangeCreat()
+    {
+        Random random;
+        random=new Random();
+        float cTemp;
+        cTemp = random.Next(1,4);//达不到最大值
+        Debug.Log(cTemp);
+        if (cTemp==1)
+        {
+            Creat(onePosition);
+
+
+        }
+        else if (cTemp==2)
+        {
+            Creat(twoPosition);
+
+
+        }
+        else if(cTemp==3)
+        {
+            Creat(threePosition);
+
+
+        }
+
+    }
+}
---
+++ 3DBirdProject/Assets/3D胖鸟/scripts/bird.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class bird : MonoBehaviour
+{
+    public Rigidbody rigBird;
+    public float jumpForce;
+    public string jumpKey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(jumpKey))
+        {
+            BirdJump();
+
+        }
+    }
+
+    public void BirdJump()
+    {
+        rigBird.AddForce(new Vector3(0, jumpForce, 0));
+    }
+
+
+}
