1191fa3cd0ae8502d950219f340c3effab99374a
1764183867@qq.com <1764183867@qq.com>
2020-05-03 12:59:51 +0800

    2020.5.3

---
+++ 3DBirdProject/Assets/3D胖鸟/scripts/ObstacleBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBase : MonoBehaviour
+{
+    public  Transform obstacleMoveT;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //obstacleMoveT.position = new Vector3(obstacleMoveT.position.x, obstacleMoveT.position.y,
+        //    obstacleMoveT.position.z * Time.deltaTime * moveSpeed);
+        obstacleMoveT.position = new Vector3(obstacleMoveT.position.x, obstacleMoveT.position.y,
+            obstacleMoveT.position.z + moveSpeed);
+
+
+    }
+}
